7cbe3175438ba3e9441112d5ffddd21495b86dac
alexander.ghyoot <alexander.ghyoot@ae.be>
2024-12-01 10:55:37 +0100

    gitignore update

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,62 @@
+
+
+#[allow(dead_code)]
+const PUZZLE_DATA: &str = "puzzle_data.txt";
+#[allow(dead_code)]
+const DATA: &str = "data.txt";
+
 fn main() {
-    println!("Hello, world!");
+    q1(PUZZLE_DATA);
+    q2(PUZZLE_DATA);
+}
+
+fn q1(filename: &str) {
+    let data = std::fs::read_to_string(filename).expect("Unable to read file");
+
+    let mut column1 = Vec::new();
+    let mut column2 = Vec::new();
+
+    for line in data.lines() {
+        let parts: Vec<&str> = line.split("   ").collect();
+        column1.push(parts[0]);
+        column2.push(parts[1]);
+    }
+
+    column1.sort();
+    column2.sort();
+
+    let mut sum = 0;
+    for (a, b) in column1.iter().zip(column2.iter()) {
+        let a: i32 = a.parse().unwrap();
+        let b: i32 = b.parse().unwrap();
+        sum += (a - b).abs();
+    }
+
+    println!("Q1: {}", sum);
+}
+
+fn q2(filename: &str){
+    let data = std::fs::read_to_string(filename).expect("Unable to read file");
+
+    let mut column1 = Vec::new();
+    let mut column2 = Vec::new();
+
+    for line in data.lines() {
+        let parts: Vec<&str> = line.split("   ").collect();
+        column1.push(parts[0]);
+        column2.push(parts[1]);
+    }
+
+    let mut similarityscore = 0;
+    for i in 0..column1.len() {
+        let mut count = 0;
+        for j in 0..column2.len() {
+            if column1[i] == column2[j] {
+                count += 1;
+            }
+        }
+        similarityscore += column1[i].parse::<i32>().expect("not a valid number") * count;
+    }
+
+    println!("Q2: {}", similarityscore);
 }
